ccf55ed58682480cb62219ba9e66b46ece82118b
Kwaku Asetena-Krah <144227247+Sunflower-dak@users.noreply.github.com>
2025-01-24 16:59:05 +0000

    commit

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function SearchForm() {
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    if (!searchValue) return;
+
+    fetch(
+      `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchValue}`
+    );
+  }, [searchValue]);
+
   return (
-    <form action="#" className="search">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      action="#"
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
         spellCheck="false"
         type="text"
         required
